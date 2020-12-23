f0c0a6aff73e4421ffd417b6c36cf01d5812af2d
Hoang Nguyen <hoangnguyen7492@gmail.com>
2020-12-23 19:50:03 +0700

    Manage Properties

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   get '/dashboard'=> 'dashboard#index', as: :dashboard
   get '/profile/:id'=> 'dashboard#profile', as: :profile
+
+  post "/agent/message" => "properties#email_agent", as: :email_agent
     
   root to: 'public#main' 
 end
