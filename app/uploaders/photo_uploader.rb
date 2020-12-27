4aeeedbef548412ced91bc43aa8e01f3460d9050
Hoang Nguyen <hoangnguyen7492@gmail.com>
2020-12-27 19:20:09 +0700

    Refactor

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,17 +1,17 @@
-class PhotoUploader < CarrierWave::Uploader::Base
-  include CarrierWave::MiniMagick
+# class PhotoUploader < CarrierWave::Uploader::Base
+#   include CarrierWave::MiniMagick
 
-  storage :fog
+#   storage :fog
 
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
+#   def store_dir
+#     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+#   end
 
-  version :thumb do
-    process :resize_to_fit => [50,50]
-  end
+#   version :thumb do
+#     process :resize_to_fit => [50,50]
+#   end
 
-  def extension_white_list
-    %w(jpg jpeg gif png)
-  end
-end
+#   def extension_white_list
+#     %w(jpg jpeg gif png)
+#   end
+# end
